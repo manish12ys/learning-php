de1d74a45e446e95c24eb8ffe60e5d124ebd2a1e
Manish yepuri <manishchowdary2006@gmail.com>
2025-04-23 10:01:30 +0530

    Update syntax.php

--- syntax.php
+++ syntax.php
@@ -1,4 +1,8 @@
 <?php
     echo "Hello, World!";
-?>
-<?= "Hello, World!" ?>
+?>//full verson of php hello world
+
+<?= "Hello, World!" ?>//short verson of hello world
+
+//to run the pogram in terminal you have to run a comand php -S localhost:8000
+
